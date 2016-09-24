fb3dd44fd1faebb410e5feecc985c8b9038092c1
John Grey <jgrey@ucsc.edu>
2016-09-23 19:15:00 -0700

    add in voronoi experiment and voronoi actual calls

--- draw_routines.py
+++ draw_routines.py
@@ -11,18 +11,25 @@ from scipy.interpolate import splprep
 from scipy.interpolate import splev
 import utils
 import IPython
+import utils
 
 #Drawing classes
 from ssClass import SandSpline
 from branches import Branches
 from voronoi import Voronoi
+from voronoiexperiments import VExperiment
 
 #constants:
 PIX = 1/pow(2,10)
 op = None
+cairo_surface = None
+filename = None
+#instances of drawing classes
 drawInstance = None
 branchInstance = None
 voronoiInstance = None
+vexpInstance = None
+
 numOfElements = 10
 iterationNum = 10
 granulate = True
@@ -32,21 +39,28 @@ interpolateGrains = False
 
 branchIterations = 100
 
-voronoi_nodes = 30
+voronoi_nodes = 10
 
 
 #top level draw command:
-def draw(ctx, drawOption,X_size,Y_size):
+def draw(ctx, drawOption,X_size,Y_size,surface=None,filenamebase="cairo_render"):
     print("Drawing: ",drawOption)
+    #modify and update globals:
     global op
     global drawInstance
     global branchInstance
     global voronoiInstance
+    global vexpInstance
+    global cairo_surface
+    global filename
+    cairo_surface = surface
+    filename = filenamebase
     op = ctx.get_operator()
     #setup the draw instances
     drawInstance = SandSpline(ctx,(X_size,Y_size))
     branchInstance = Branches(ctx,(X_size,Y_size))
     voronoiInstance = Voronoi(ctx,(X_size,Y_size),voronoi_nodes)
+    vexpInstance = VExperiment(ctx,(X_size,Y_size),voronoi_nodes)
     #ctx.set_operator(OPERATOR_SOURCE)
     utils.clear_canvas(ctx)
 
@@ -70,9 +84,14 @@ def draw(ctx, drawOption,X_size,Y_size):
         drawBranch(X_size,Y_size)
     elif drawOption == "voronoi":
         drawVoronoi(X_size,Y_size)
+    elif drawOption == "vexp":
+        drawVExp(X_size,Y_size)
     else:
         raise Exception("Unrecognized draw routine",drawOption)
 
+    if surface:
+        utils.write_to_png(surface,filenamebase)
+    
 #step the drawing deformation:
 def iterateAndDraw():
     for i in range(iterationNum):
@@ -119,6 +138,35 @@ def drawBranch(X_size,Y_size):
     branchInstance.draw()
 
 def drawVoronoi(X_size,Y_size):
+    """ Step through the construction of a voronoi diagram """
+    i = 0
+    result = True
     voronoiInstance.initGraph()
-    voronoiInstance.calculate()
-    voronoiInstance.draw()
+    voronoiInstance.draw_intermediate_states()
+    utils.write_to_png(cairo_surface,filename,i)
+    while result:
+        i += 1
+        result = voronoiInstance.calculate()
+        voronoiInstance.draw_intermediate_states()
+        utils.write_to_png(cairo_surface,filename,i)
+        if i > 150:
+            #rough infinite loop guard
+            raise Exception('Voronoi has run too long')
+
+    #calculations finished, get the final DCEL:
+    dcel = voronoiInstance.finalise_DCEL()
+    #todo: draw the final dcel
+    voronoiInstance.draw_voronoi_diagram()
+    finalFilename = "{}-FINAL".format(filename)
+    utils.write_to_png(cairo_surface,finalFilename,i+1)
+    
+    
+def drawVExp(X_size,Y_size):
+    vexpInstance.drawTest()
+    
+
+def example_multi_render(Xs,Ys):
+    for i in range(1000):
+        #do something
+        if i%10:
+            utils.write_to_png(surface,filename,i=i)
