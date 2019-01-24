5c8c01f8fb48b8ae2f7a0580dfa2c5be7bae28fb
John Grey <jgrey@ucsc.edu>
2019-01-24 02:32:23 +0000

    defined a simple initial pipeline that generates some shapes

--- cairo_splines/example_pipeline.py
+++ cairo_splines/example_pipeline.py
@@ -1,12 +1,13 @@
+"""
+An initial generating pipeline layer
+"""
 import numpy as np
 import cairo_utils as utils
-from .simple_draw import SimpleDraw
+from .pdraw import PDraw
 
-
-
-def ExamplePipeline(d, pipeline_data):
+def ExamplePipeline(d, opts, data):
     """ The simplest pipeline. adds some shapes, and samples """
-    assert(isinstance(d, SimpleDraw))
+    assert(isinstance(d, PDraw))
 
     verts = np.random.random((20,7))
     #scale up the position and radius: ((b - a) * x) + a
@@ -16,32 +17,27 @@ def ExamplePipeline(d, pipeline_data):
     points = (verts * mask)
     # d.add_points(points)
 
-    d.add_circle(np.array([[d._center[0], d._center[1],
-                               0, utils.constants.TWOPI,
-                               200, 800,
-                               0,1,1,0.4
-    ]]))
+    # d.add_circle(np.array([[d._center[0], d._center[1],
+    #                            0, utils.constants.TWOPI,
+    #                            200, 800,
+    #                            0,1,1,0.4
+    # ]]))
 
-    d.add_points(np.array([[d._center[0], d._center[1],300, 1, 0, 0, 0.4],
-                              [0,0,150, 0, 0, 1,0.3],
-                              [0,d._size[1],150, 0, 0.5, 0.5, 0.3],
-                              [d._size[0],0,150, 0.5, 0.5, 0, 0.3]]))
-
-    d.add_bezier(np.array([[d._center[0] - d._size[0]*0.5, d._center[1],
-                               d._center[0], d._center[1] + d._size[1]*0.25,
-                               d._center[0] + d._size[0]*0.25, d._center[1] - d._size[1]*0.25,
-                               d._center[0] + d._size[0]*0.5, d._center[1],
-                               1,0.2,0.4,0.1]]))
+    # d.add_points(np.array([[d._center[0], d._center[1],300, 1, 0, 0, 0.4],
+    #                        [0,0,150, 0, 0, 1,0.3],
+    #                        [0,d._size[1],150, 0, 0.5, 0.5, 0.3],
+    #                        [d._size[0],0,150, 0.5, 0.5, 0, 0.3]]))
 
     d.add_bezier(np.array([[d._center[0] - d._size[0]*0.5, d._center[1],
-                               d._center[0], d._center[1] + d._size[1]*0.25,
-                               d._center[0] + d._size[0]*0.25, d._center[1] - d._size[1]*0.25,
-                               d._center[0] + d._size[0]*0.5, d._center[1],
-                               1,0.2,0.4,0.2]]))
-    d.sample_shapes(3800, r=3)
+                            d._center[0], d._center[1] + d._size[1]*0.25,
+                            d._center[0] + d._size[0]*0.25, d._center[1] - d._size[1]*0.25,
+                            d._center[0] + d._size[0]*0.5, d._center[1],
+                            1,0.2,0.4,0.1]]))
 
+    d.add_bezier(np.array([[d._center[0], d._center[1] - 2000,
+                            d._center[0] - 2000, d._center[1] + d._size[1]*0.25,
+                            d._center[0] + d._size[0]*0.25, d._center[1] - d._size[1]*0.25,
+                            d._center[0] + d._size[0]*0.5, d._center[1] + 2000,
+                            0.3,0.8,0.5,0.1]]))
 
-    d.add_text("Example Draw", [d._size[0]*0.1, d._size[1]*0.05],
-                    d._size[0]*0.02, [1,1,1,1])
-
-    return None
+    return data
