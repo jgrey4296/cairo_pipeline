74190a6fa874a043eec30fe5155e153e0a7cdafc
John Grey <jgrey@ucsc.edu>
2019-01-24 02:30:50 +0000

    updated init for new layers/pipelines

--- cairo_splines/__init__.py
+++ cairo_splines/__init__.py
@@ -1,3 +1,4 @@
-from .simple_draw import SimpleDraw
-from .example_draw import ExampleDraw
+from .pdraw import PDraw
+from .example_pipeline import ExamplePipeline
 from .colour_pipeline import ColourPipeline
+from .sand_deformation import SandPipeline
