4b197025ca9639a7f7ffcc18e0dc61c0213f13f6
John Grey <jgrey@ucsc.edu>
2019-01-30 06:09:49 +0000

    cleaned up pdraw, removed unnecessary functions, majorly improved pipeline method

--- cairo_splines/pdraw.py
+++ cairo_splines/pdraw.py
@@ -8,6 +8,9 @@ import cairo_utils as utils
 import IPython
 from . import constants
 
+import logging as root_logger
+logging = root_logger.getLogger(__name__)
+
 class PDraw:
     """ Draws a Pipeline of operations """
 
@@ -20,33 +23,68 @@ class PDraw:
         self._surface = surface
         self._size = sizeTuple
         self._center = (sizeTuple[0] * 0.5, sizeTuple[1] * 0.5)
-        #Core points : np.array(x,y,rad,r,g,b,a)
-        self._core_verts = np.zeros((1,constants.SAMPLE_DATA_LEN))
         #More Complex shapes
-        self._lines = np.zeros((1,constants.LINE_DATA_LEN))
-        # [p1, cp1, cp2, p2]
-        self._beziers = np.zeros((1,constants.BEZIER_DATA_LEN))
+        self._geometry = {
+            'points' : np.zeros((1,utils.constants.SAMPLE_DATA_LEN)),
+            'line' : np.zeros((1,utils.constants.LINE_DATA_LEN)),
+            # [p1, cp1, cp2, p2]
+            'bezier' : np.zeros((1,utils.constants.BEZIER_DATA_LEN)),
         # [p, min_radius, max_radius, min_rads, max_rads]
-        self._circles = np.zeros((1,constants.CIRCLE_DATA_LEN))
+            'circle' : np.zeros((1,utils.constants.CIRCLE_DATA_LEN))
+        }
+        self._sampled_geometry = {
+            'line' : np.zeros((1,utils.constants.LINE_DATA_LEN)),
+            # [p1, cp1, cp2, p2]
+            'bezier' : np.zeros((1,utils.constants.BEZIER_DATA_LEN)),
+            # [p, min_radius, max_radius, min_rads, max_rads]
+            'circle' : np.zeros((1,utils.constants.CIRCLE_DATA_LEN))
+        }
         #additional data
-        self._samples = np.zeros((1,constants.SAMPLE_DATA_LEN))
-        self._noise = None
-        self._stack = []
+        self._current = np.zeros((1, utils.constants.SAMPLE_DATA_LEN))
+        self._samples = np.zeros((1,utils.constants.SAMPLE_DATA_LEN))
         self._text = []
+        #Registered layers that can be called from within a pipeline layer
+        self._registered_call= {}
+
         # Lookup of Objects associated with basic data
         # eg: _nodes['_core_verts'][1] can hold an object associated with _core_verts[0]
         self._nodes = {}
         # Debug flag for layers:
         self._debug= False
 
-    def pipeline(self, pipelines):
+    def register_call(self, name, func, start_state=None):
+        """ Registers a callable function, and its personal data store """
+        if state_state is None:
+            start_state = {}
+        self._registered_call[name] = (func, start_state)
+
+    def call(self, name, args):
+        """ Calls a registered_call by name, with a dictionary of arguments, and its personal state
+        The call returns the result of the calculation, and an updated personal state """
+        func, state = self._registered_call[name]
+        result, new_state = func(args, state)
+        self.register_call(name, func, new_state)
+        return result
+
+    def pipeline(self, pipelines, max_loops=10):
         """ Transforms the drawing in a set of steps """
-        pipeline_data = {}
-        pipe_pairs = zip(islice(pipelines, 0, len(pipelines), 2),
-                         islice(pipelines, 1, len(pipelines), 2))
-        for (x, opts) in pipe_pairs:
-            pipeline_data = x(self, opts, pipeline_data)
-        return pipeline_data
+        data = { 'current_step' : 0,
+                 'finish' : False,
+                 'loop_num' : 0,
+                 'max_loops' : max_loops}
+        pipe_pairs = list(zip(islice(pipelines, 0, len(pipelines), 2),
+                              islice(pipelines, 1, len(pipelines), 2)))
+        pipeline_length = len(pipe_pairs)
+        while data['current_step'] < pipe_pairs \
+              and not data['finish']:
+            #####
+            x, opts = pipe_pairs[data['current_step']]
+            logging.info("Running Layer: ({}) {}".format(len(self._samples), x.__name__))
+            data = x(self, opts, data)
+            data['current_step'] += 1
+            ####
+
+        return data
 
     #------------------------------
 	# def Draw Primitives
@@ -54,36 +92,38 @@ class PDraw:
 
     def add_points(self, points):
         """ Add 2 dimensional points to the data to draw,
-        points: np.array([[x,y,r,r,g,b,a]])
+        points: np.array([[x,y,r]])
         """
         assert(isinstance(points, np.ndarray))
-        assert(points.shape[1] == constants.SAMPLE_DATA_LEN)
-        self._core_verts = np.row_stack((self._core_verts, points))
+        assert(points.shape[1] == utils.constants.SAMPLE_DATA_LEN)
+        self._geometry['point'] = np.row_stack((self._geometry['point'], points))
 
     def add_lines(self, lines):
         """ Add lines to sample then draw
-        lines: np.array([[x,y,x,y,r,g,b,a]])
+        lines: np.array([[x,y,x,y]])
         """
         assert(isinstance(lines, np.ndarray))
-        assert(lines.shape[1] == constants.LINE_DATA_LEN)
-        self._lines = np.row_stack((self._lines, lines))
+        assert(lines.shape[1] == utils.constants.LINE_DATA_LEN)
+        self._geometry['line'] = np.row_stack((self._geometry['line'],
+                                               lines))
 
     def add_bezier(self, beziers):
         """ Add 2 cp bezier curves to sample then draw
-        beziers: np.array([[x1,y1,cx1, cy1, cx2, cy2, x2, y2, r, g, b, a]])
+        beziers: np.array([[x1,y1,cx1, cy1, cx2, cy2, x2, y2]])
         """
         assert(isinstance(beziers, np.ndarray))
-        assert(beziers.shape[1] == constants.BEZIER_DATA_LEN)
-        self._beziers = np.row_stack((self._beziers, beziers))
+        assert(beziers.shape[1] == utils.constants.BEZIER_DATA_LEN)
+        self._geometry['bezier'] = np.row_stack((self._geometry['bezier'],
+                                                 beziers))
 
     def add_circle(self, circles):
         """ Add circles to sample then draw
-        Circles: np.array([[x,y, rad_min, rad_max, radius_min, radius_max,
-        r, g, b, a]])
+        Circles: np.array([[x,y, rad_min, rad_max, radius_min, radius_max]])
         """
         assert(isinstance(circles, np.ndarray))
-        assert(circles.shape[1] == constants.CIRCLE_DATA_LEN)
-        self._circles = np.row_stack((self._circles, circles))
+        assert(circles.shape[1] == utils.constants.CIRCLE_DATA_LEN)
+        self._geometry['circle'] = np.row_stack((self._geometry['circle'],
+                                                 circles))
 
     def add_text(self, text, position, size, colour):
         """ Adds text on top of the drawing """
@@ -98,62 +138,6 @@ class PDraw:
             self._ctx.set_font_size(s)
             self._ctx.set_source_rgba(*c)
             utils.drawing.draw_text(self._ctx, p, t)
-
-
-    #------------------------------
-    # def Sampling
-    #------------------------------
-
-    def sample_shapes(self, n, r=10, types=None):
-        """ Sample along defined shapes """
-        if types is None:
-            types = ['line','bezier','circle']
-        if 'line' in types:
-            self.sample_lines(n, r=r)
-        if 'bezier' in types:
-            self.sample_beziers(n, r=r)
-        if 'circle' in types:
-            self.sample_circles(n, r=r)
-
-    def sample_lines(self, n, r=10):
-        #sample lines
-        ts = np.linspace(0,1,n)
-        if len(self._lines) > 1:
-            self._samples = np.row_stack((self._samples,
-                                          utils.umath.sample_wrapper(
-                                              utils.umath.sample_along_lines,
-                                              self._lines[1:,:-constants.COLOUR_SIZE],
-                                              ts,
-                                              r,
-                                              self._lines[1:,-constants.COLOUR_SIZE:])
-            ))
-
-    def sample_beziers(self, n, r=10):
-        #sample beziers
-        ts = np.linspace(0,1,n)
-        if len(self._beziers) > 1:
-            self._samples = np.row_stack((self._samples,
-                                          utils.umath.sample_wrapper(
-                                              utils.umath.bezier2cp,
-                                              self._beziers[1:,:-constants.COLOUR_SIZE],
-                                              n,
-                                              r,
-                                              self._beziers[1:,-constants.COLOUR_SIZE:])
-            ))
-
-    def sample_circles(self, n, r=10):
-        #sample circles - on diameter
-        ts = np.linspace(0,1,n)
-        if len(self._circles) > 1:
-            f = lambda x,c: utils.umath.sample_circle(x,c,sort_rads=False, sort_radi=False)
-            self._samples = np.row_stack((self._samples,
-                                          utils.umath.sample_wrapper(
-                                              f,
-                                              self._circles[1:,:-constants.COLOUR_SIZE],
-                                              n,
-                                              r,
-                                              self._circles[1:,-constants.COLOUR_SIZE:])
-            ))
 
 
     #------------------------------
