5d6d956d3c41a00703c7571894a6423c32f787db
John Grey <jgrey@ucsc.edu>
2019-01-30 12:58:55 +0000

    massively simplified colour pipeline module

--- cairo_splines/colour_pipeline.py
+++ cairo_splines/colour_pipeline.py
@@ -5,47 +5,24 @@ import numpy as np
 import cairo_utils as utils
 from . import constants
 
-def ColourPipeline(d, opts, data):
+def hue_rotate(d, opts, data):
     """ Create a gentle variation of colours """
+    assert(data['c_type'] == 'hsla')
     n = data['n']
-    speed = opts['speed']
     easing = utils.easings.lookup(opts['easing'])
-
     samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
-    ones = np.ones(n)
-    c_i = (np.linspace(0, 1, n))
-    rndMax = opts['rndsig'][1] - opts['rndsig'][0]
-    results = np.zeros((1, 7))
+    results = np.zeros((1, utils.constants.SAMPLE_DATA_LEN))
 
     for sample_set in samples:
         non_colour = sample_set[:, :-utils.constants.COLOUR_SIZE]
         colours = sample_set[:, -utils.constants.COLOUR_SIZE:]
 
-
-        rnd = easing(opts['rndsig'][0] + (np.random.random((n)) * rndMax) , 0)
-        sig = ((c_i + rnd) * utils.constants.TWOPI) * opts['scale']
-        cos_c = np.cos(sig)
-        sin_c = np.sin(sig)
-        inter_c = cos_c * sin_c
-
-        combined = np.column_stack((cos_c, sin_c, inter_c, ones))
-
-        col_prime = opts['base'] + (colours * combined)
-        sig_col = easing(col_prime, 0, r=[0,1])
-        col_prime2 = utils.easings.quantize(sig_col)
-
-        target = col_prime
-        if opts['target'] == 0:
-            target = sig_col
-        if opts['target'] == 1:
-            target = col_prime2
-
+        noise = d.call('random', { 'range' : opts['noise'], 'shape': (n,1)})
+        zeros = np.zeros((n,3))
+        n_zzz = np.column_stack((noise, zeros))
+        colours += n_zzz
         results = np.row_stack((results,
-                                np.column_stack((non_colour, target))))
+                                np.column_stack((non_colour, colours))))
 
-    if 'alpha' in opts:
-        results[:,-1] = opts['alpha']
-    if 'radius' in opts:
-        results[:, -(utils.constants.COLOUR_SIZE + 1)] = opts['radius']
-    d._samples = results[1:]
+    d._samples = results
     return data
