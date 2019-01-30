801f8ebea542441dffd21890728398a1e74b7496
John Grey <jgrey@ucsc.edu>
2019-01-30 06:10:40 +0000

    improved and simplified sand deformation layers

--- cairo_splines/sand_deformation.py
+++ cairo_splines/sand_deformation.py
@@ -3,51 +3,74 @@ A Drawing class based on inconvergents sandsplines
 """
 import numpy as np
 from numpy.random import random
+from sklearn.preprocessing import normalize
 import cairo_utils as utils
 import IPython
 from .pdraw import PDraw
 from . import constants
 import IPython
 
-def SandPipeline(d, opts, data):
+
+def displace_layer(d, opts, data):
     """ Treats data as sequences to be displaced by noisy sine signals """
     n = data['n']
-    easing = utils.easings.lookup(opts['easing'])
-    easing_2 = utils.easings.lookup('pow_cos_pi')
-    ramp = easing(np.linspace(0,1,n), 0)
-    ramp2 = easing_2(np.linspace(-1,1,n), 3.5)
-
+    easing = utils.easings.lookup(opts['easing'][0])
+    easing_2 = utils.easings.lookup(opts['easing'][1])
+    ramp = easing(np.linspace(-1,1,n), 0)
+    ramp2 = easing_2(np.linspace(-1,1,n), 0.5)
     scale = opts['scale']
-    samples = d._samples[1:].reshape((-1, n, constants.SAMPLE_DATA_LEN))
-    result = np.zeros((1,constants.SAMPLE_DATA_LEN))
+    samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
+    result = np.zeros((1,utils.constants.SAMPLE_DATA_LEN))
 
     for sample_set in samples:
         xys = sample_set[:,:2]
-        rst = sample_set[:,2:].repeat(opts['channels'], axis=0)
+        rst = sample_set[:,2:]
 
         # base random signal
         # TODO: make this a separate layer? make it FM or AM or additive?
-        noise = easing((1.0-2.0 * random((opts['channels'], 1, 1))), 0)
-        rads = (ramp * opts['speed']) * ramp2 * utils.umath.TWOPI * noise
-        rotation = np.column_stack((-np.cos(opts['phase'] + rads), np.sin(opts['phase'] + rads)))
-        combined = rotation * scale * noise
+        noise = easing((1.0-2.0) * np.random.random(), 0) * opts['noise_mul']
+        speed = opts['speed'][0] + (np.random.random() * (opts['speed'][1] - opts['speed'][0]))
+        phase = opts['phase'][0] + (np.random.random() * (opts['phase'][1] - opts['phase'][0]))
 
-        formatted = combined.reshape((opts['channels'], n, 2), order='F')
+        rads = ramp2 * (speed + noise) * utils.umath.TWOPI
+        rads_reshaped = rads.reshape((n, 1))
+        rotation = np.hstack((-np.cos(phase + rads_reshaped),
+                              np.sin(phase + rads_reshaped)))
+        combined = rotation * scale
 
-        i_xys = (xys + formatted).reshape((-1,2))
+        formatted = combined.T
+        i_xys = (xys + combined).reshape((-1,2))
         recombined = np.column_stack((i_xys, rst))
 
         #recombine
         result = np.row_stack((result, recombined))
 
-    d._samples = np.row_stack((d._samples, result[1:]))
+    d._samples = np.row_stack((np.zeros((1, utils.constants.SAMPLE_DATA_LEN),
+                                        result)))
     return data
 
 def granulate_layer(d, opts, data):
-    currentGrains = utils.math.granulate(points, grains=grains, mult=grainMult)
-    #points = utils.math._interpolate(points, sampleSize, smoothing=d.smooth)
-    if interpolateGrains:
-        currentGrains = utils.math._interpolate(currentGrains, d.interpolationPoints, smoothing=d.smooth)
-        d.calculatedGrains.append(currentGrains)
+    n = data['n']
+    rad = opts['rad']
+    samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
+    result = np.zeros((1, utils.constants.LINE_DATA_LEN))
 
+    for sample_set in samples:
+        xys = sample_set[:,:2]
+        rst = sample_set[:,2:]
+        ds = xys[:-1] - xys[1:]
+        dirs = np.arctan2(ds[:,1], ds[:,0])
+        rads = rad[0] + (np.random.random(dirs.shape) * (rad[1] - rad[0]))
+
+        dirs_prime = dirs + rads
+        rot_vector = np.array([np.cos(dirs_prime), np.sin(dirs_prime)]).T
+        dist_scale = opts['mult'][0] + \
+            (np.random.random(rot_vector.shape) * (opts['mult'][1] - opts['mult'][0]))
+        scaled_rot = rot_vector * dist_scale
+
+        grain_line = np.column_stack((xys[:-1], xys[:-1] + scaled_rot))
+
+        result = np.row_stack((result, grain_line))
+
+    d.add_lines(result)
     return data
