64f56e757fba0fcdf4c5a75caa8d94ae1e8fbaf8
John Grey <jgrey@ucsc.edu>
2019-01-30 13:01:05 +0000

    fiddled with sand_deformation

--- cairo_splines/sand_deformation.py
+++ cairo_splines/sand_deformation.py
@@ -16,9 +16,8 @@ def displace_layer(d, opts, data):
     n = data['n']
     easing = utils.easings.lookup(opts['easing'][0])
     easing_2 = utils.easings.lookup(opts['easing'][1])
-    ramp = easing(np.linspace(-1,1,n), 0)
-    ramp2 = easing_2(np.linspace(-1,1,n), 0.5)
-    scale = opts['scale']
+    ramp = easing(np.linspace(0,1,n), 0).reshape((n,1))
+    ramp2 = easing_2(np.linspace(-1,1,n), 3.5).reshape((n,1))
     samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
     result = np.zeros((1,utils.constants.SAMPLE_DATA_LEN))
 
@@ -28,25 +27,24 @@ def displace_layer(d, opts, data):
 
         # base random signal
         # TODO: make this a separate layer? make it FM or AM or additive?
-        noise = easing((1.0-2.0) * np.random.random(), 0) * opts['noise_mul']
-        speed = opts['speed'][0] + (np.random.random() * (opts['speed'][1] - opts['speed'][0]))
-        phase = opts['phase'][0] + (np.random.random() * (opts['phase'][1] - opts['phase'][0]))
+        noise = easing(d.call('random', {'range': np.array([-1,1]) * opts['noise_mul'],
+                                         'shape': (n,1) }), 0)
+        speed = d.call('random', { 'range' : opts['speed'], 'shape' : (1, 1)})
+        phase = d.call('random', { 'range' : opts['phase'], 'shape' : (1, 1)})
+        scale = d.call('random', { 'range' : opts['scale'], 'shape' : (n, 1)})
 
-        rads = ramp2 * (speed + noise) * utils.umath.TWOPI
+        rads = (ramp2 * (speed * utils.umath.TWOPI)) + noise
         rads_reshaped = rads.reshape((n, 1))
-        rotation = np.hstack((-np.cos(phase + rads_reshaped),
+        rotation = np.hstack((np.cos(phase + rads_reshaped),
                               np.sin(phase + rads_reshaped)))
         combined = rotation * scale
-
-        formatted = combined.T
-        i_xys = (xys + combined).reshape((-1,2))
+        i_xys = (xys + combined)
         recombined = np.column_stack((i_xys, rst))
-
         #recombine
         result = np.row_stack((result, recombined))
 
-    d._samples = np.row_stack((np.zeros((1, utils.constants.SAMPLE_DATA_LEN),
-                                        result)))
+    d._samples = np.row_stack((np.zeros((1, utils.constants.SAMPLE_DATA_LEN)),
+                               result[1:]))
     return data
 
 def granulate_layer(d, opts, data):
@@ -60,12 +58,11 @@ def granulate_layer(d, opts, data):
         rst = sample_set[:,2:]
         ds = xys[:-1] - xys[1:]
         dirs = np.arctan2(ds[:,1], ds[:,0])
-        rads = rad[0] + (np.random.random(dirs.shape) * (rad[1] - rad[0]))
+        rads = d.call('random', { 'range': rad, 'shape': dis.shape })
 
         dirs_prime = dirs + rads
         rot_vector = np.array([np.cos(dirs_prime), np.sin(dirs_prime)]).T
-        dist_scale = opts['mult'][0] + \
-            (np.random.random(rot_vector.shape) * (opts['mult'][1] - opts['mult'][0]))
+        dist_scale = d.call('random', { 'range' : opts['mult'], 'shape': rot_vector.shape })
         scaled_rot = rot_vector * dist_scale
 
         grain_line = np.column_stack((xys[:-1], xys[:-1] + scaled_rot))
