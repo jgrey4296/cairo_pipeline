b21bd6b98587f14712f308789cf334e0d52d9f92
John Grey <jgrey@ucsc.edu>
2019-02-12 19:18:02 +0000

    improved top level __init__

--- cairo_pipeline/__init__.py
+++ cairo_pipeline/__init__.py
@@ -1,1 +1,8 @@
 from .pdraw import PDraw
+
+from . import colour as colour
+from . import dcelp as dcelp
+from . import growth as growth
+from . import misc as misc
+from . import noise as noise
+from . import vertical_calls as calls
