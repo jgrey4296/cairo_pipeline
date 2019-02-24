dd2caaf8f0a8f89667c8467b7cfbddd755e93c79
John Grey <jgrey@ucsc.edu>
2019-02-24 02:43:57 +0000

    update of sampling layer to use crosscuts

--- cairo_pipeline/misc/sampling.py
+++ cairo_pipeline/misc/sampling.py
@@ -14,47 +14,66 @@ def sample_layer(d, opts, data):
     """ Layer that samples a specific set of geometry
     Parameters: n, r, colour, target, choice, easing
     """
-    if 'n' not in opts and 'n' not in data:
-        raise Exception('n is not specified in sampling')
-    if 'n' not in data:
-        data['n'] = opts['n']
-    r = opts['r']
-    colour = opts['colour']
-    data['c_type'] = opts['c_type']
-    target_data = d._geometry[opts['target']]
-    target_sampler = constants.SAMPLER_LOOKUP[opts['target']]
-    easing = None
-    if 'easing' in opts:
-        easing = opts['easing']
+    vals, data = d.call_crosscut('access',
+                        lookup={
+                            'c_type': 'hsla',
+                            'choice': False,
+                            'colour': np.array([0,0,1,1]),
+                            'easing': ['static',1],
+                            'n': 5,
+                            'r': 3,
+                            'random': ['uniform', {}],
+                            'target': 'circle',
+                        },
+                        opts=opts , data=data)
+    c_type, choice, colour, easing, n, r, random_params, target = vals
+    random, data = d.call_crosscut('access',
+                                   namespace='random',
+                                   key=random_params[0],
+                                   params=random_params[1],
+                                   opts=opts, data=data)
+    #TODO: Change this to crosscuts
+    target_data = d._geometry[target]
+    target_sampler = constants.SAMPLER_LOOKUP[target]
+
     if len(target_data) < 1:
         return data
 
-    # from https://stackoverflow.com/questions/14262654/
-    if opts['choice'] != -1:
-        target_data = target_data[np.random.choice(target_data.shape[0],
-                                                   size=int(len(target_data) * opts['choice'])),
-                                  :]
+    if choice:
+        target_data = target_data[choice(target_data.shape[0]), :]
 
     new_samples = utils.umath.sample_wrapper(target_sampler,
                                              target_data[1:],
-                                             data['n'],
+                                             n,
                                              r,
                                              colour,
-                                             easing=easing)
+                                             easing=easing,
+                                             random=random)
 
     d._samples = np.row_stack((d._samples, new_samples))
-    d._sampled_geometry[opts['target']] = np.row_stack((d._sampled_geometry[opts['target']],
+    d._sampled_geometry[target] = np.row_stack((d._sampled_geometry[target],
                                                         target_data))
-    d._geometry[opts['target']] = np.zeros((1, target_data.shape[1]))
+    d._geometry[target] = np.zeros((1, target_data.shape[1]))
+
+    d.call_crosscut('store', pairs={'n': n,
+                                    'c_type' : c_type },
+                    target='data', data=data)
+
     return data
 
 def subsample_layer(d, opts, data):
-    n = opts['n']
-    section_num = opts['sections'] + 1
-    end_points = np.linspace(0,data['n'],section_num, dtype=int)
+    vals, data = d.call_crosscut('access',
+                                 lookup={'n': 5,
+                                         'sections': 1},
+                                 opts=opts,
+                                 data=data)
+    n, sections = vals
+    sections += 1
+
+    end_points = np.linspace(0, n, section_num, dtype=int)
     segments = list(zip(end_points[:-1],end_points[1:]))
     subsample = np.zeros((1, utils.constants.SAMPLE_DATA_LEN))
-    samples = d._samples[1:].reshape((-1, data['n'], utils.constants.SAMPLE_DATA_LEN))
+    samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
     for (i, sampleset) in enumerate(samples):
         xys = sampleset[:,:2]
         rst = sampleset[:,2:]
