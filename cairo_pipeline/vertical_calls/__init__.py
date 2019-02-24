55409a39743acd38eebcc6e2cc32c2e00e55ed4f
John Grey <jgrey@ucsc.edu>
2019-02-24 02:41:59 +0000

    removal of obsolete files

--- cairo_pipeline/vertical_calls/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import easing as easing
-from . import no_op as no_op
-from . import random as random
-from . import value_control as value_control
-from .setup_calls import register_calls
--- cairo_pipeline/vertical_calls/setup_calls.py
+++
@@ -1,9 +0,0 @@
-"""
-Defines a layer to register vertical calls, within a pipeline
-"""
-def register_calls(d, opts, data):
-    """ A Simple layer to register a dictionary of functions into
-    the draw object for use by later layers """
-    for key,func in opts.items():
-        d.register_call(key, func)
-    return data
