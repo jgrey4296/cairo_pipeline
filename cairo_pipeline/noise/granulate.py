43bd03d298e822b221bfa81121b283c0f14bdb3a
John Grey <jgrey@ucsc.edu>
2019-02-24 02:44:45 +0000

    update of granulate layer to use crosscuts

--- cairo_pipeline/noise/granulate.py
+++ cairo_pipeline/noise/granulate.py
@@ -15,8 +15,17 @@ logging = root_logger.getLogger(__name__)
 
 def granulate(d, opts, data):
     """ A Layer to granulate a signal of samples """
-    n = data['n']
-    rad = opts['rad']
+    vals, data = d.call_crosscut('access',
+                                 lookup={
+                                     'dist_range' : [0,1],
+                                     'n'    : 5,
+                                     'override_dir' : False,
+                                     'rad_range'  : [0, 3.142],
+                                     'random' : ['uniform', {}],
+                                 },
+                                 opts=opts, data=data)
+    dist_range, n, override_dir, rad_range, random_args= vals
+
     samples = d._samples[1:].reshape((-1, n, utils.constants.SAMPLE_DATA_LEN))
     result = np.zeros((1, utils.constants.LINE_DATA_LEN))
 
@@ -30,18 +39,26 @@ def granulate(d, opts, data):
         rst = sample_set[:,2:]
         ds = xys[:-1] - xys[1:]
         dirs = np.arctan2(ds[:,1], ds[:,0])
-        rads, discard = d.call('random', { 'range': rad, 'shape': dirs.shape })
-
+        noise, data = d.call_crosscut('call',
+                                      namespace='random',
+                                      key=random_args[0],
+                                      params=random_args[1],
+                                      shape=dirs.shape,
+                                      opts=opts, data=data)
+        #TODO: Standardize scaling
+        rads = rad_range[0] + (rad_range[1] * noise)
         dirs_prime = dirs + rads
         rot_vector = np.array([np.cos(dirs_prime), np.sin(dirs_prime)]).T
-        dist_scale, discard  = d.call('random', { 'range' : opts['mult'],
-                                                  'shape': rot_vector.shape })
-        scaled_rot = rot_vector * dist_scale
-
+        noise, discard  = d.call_crosscut('call',
+                                          namespace='random',
+                                          key=random_args[0],
+                                          params=random_args[1],
+                                          shape=rot_vector.shape,
+                                          opts=opts, data=data)
+        rand_dist = dist_range[0] + (noise * dist_range[1])
+        scaled_rot = rot_vector * rand_dist
         grain_line = np.column_stack((xys[:-1], xys[:-1] + scaled_rot))
-
         result = np.row_stack((result, grain_line))
-
 
     d.add_lines(result)
     return data
